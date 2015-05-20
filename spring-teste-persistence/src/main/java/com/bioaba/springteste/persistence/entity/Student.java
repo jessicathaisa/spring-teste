8bcd3a5d3a05b07e61464eb833c3c5c299e807e5
Jessica Thaisa Silva de Almeida <jessicatsalmeida@gmail.com>
2015-05-20 10:41:33 -0300

    Modificações no repositorio e na classe Student

--- spring-teste-persistence/src/main/java/com/bioaba/springteste/persistence/entity/Student.java
+++ spring-teste-persistence/src/main/java/com/bioaba/springteste/persistence/entity/Student.java
@@ -1,14 +1,17 @@
 package com.bioaba.springteste.persistence.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import com.bioaba.springteste.persistence.entity.base.AbstractEntity;
 
 @Entity
-@Table(name = "TB_STUDENT", uniqueConstraints = @UniqueConstraint(columnNames = { "name" }))
+@Table(name = "student")
 public class Student extends AbstractEntity{
+	
 	private String name;
 
 	public String getName() {
--- spring-teste-persistence/src/main/java/com/bioaba/springteste/persistence/repository/IStudentRepository.java
+++ spring-teste-persistence/src/main/java/com/bioaba/springteste/persistence/repository/IStudentRepository.java
@@ -1,13 +1,16 @@
 package com.bioaba.springteste.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bioaba.springteste.persistence.entity.Student;
 
 @Repository
 public interface IStudentRepository extends JpaRepository<Student, Long> {
-
-	public Student findByName(String name);
+	
+	public List<Student> findAll();
 	
 }
